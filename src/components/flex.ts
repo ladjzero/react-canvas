ce3d057ba46bde771861c498ff8fe40fef8a63de
ladjzero <ladjzero@163.com>
2020-05-07 23:26:36 +0800

    feat(flex): justifyContent: space-between

--- src/components/flex.ts
+++ src/components/flex.ts
@@ -1,22 +1,58 @@
 import { Node } from './node';
-import { FlexLayoutProps } from './flexProps';
 
 export default class FlexLayout extends Node {
-  props: FlexLayoutProps
-
   layout(x: number = 0, y: number = 0, w: number = this.props?.style?.width, h: number = this.props?.style?.height) {
     const flexDirection = this.props?.style?.flexDirection;
+    const justifyContent = this.props?.style?.justifyContent;
 
     if (flexDirection === 'row') {
+      let childrenWidthSum = 0;
+      let xpointer = x;
+
       this.children.forEach(c => {
         const layoutRect = c.layout(x, y);
+        childrenWidthSum += layoutRect.w;
         x += layoutRect.w;
       });
+
+      if (justifyContent) {
+        switch (justifyContent) {
+          case 'space-between': {
+            if (this.children.length > 1) {
+              const space = (w - childrenWidthSum) / (this.children.length - 1);
+              xpointer = x;
+              this.children.forEach(c => {
+                const layoutRect = c.layout(xpointer, y);
+                xpointer += layoutRect.w + space;
+              });
+            }
+          }
+        }
+      }
     } else {
+      let childrenHeightSum = 0;
+      let ypointer = y;
+
       this.children.forEach(c => {
-        const layoutRect = c.layout(x, y);
-        y += layoutRect.h;
+        const layoutRect = c.layout(x, ypointer);
+        childrenHeightSum += layoutRect.h;
+        ypointer += layoutRect.h;
       });
+
+      if (justifyContent) {
+        switch (justifyContent) {
+          case 'space-between': {
+            if (this.children.length > 1) {
+              const space = (h - childrenHeightSum) / (this.children.length - 1);
+              ypointer = y;
+              this.children.forEach(c => {
+                const layoutRect = c.layout(x, ypointer);
+                ypointer += layoutRect.h + space;
+              });
+            }
+          }
+        }
+      }
     }
     this.x = x;
     this.y = y;
--- src/examples/flex.stories.tsx
+++ src/examples/flex.stories.tsx
@@ -11,7 +11,24 @@ export const directionColumn = () => {
       <flex style={{ height: 200, width: 200 }}>
         <view style={{ height: 60, width: 60, backgroundColor: 'red' }}>60 x 60</view>
         <view style={{ height: 20, width: 100, backgroundColor: 'blue' }}>20 x 100</view>
-        <view style={{ height: 60, width: 60, backgroundColor: 'green' }}>60 x 60</view>
+        <view style={{ height: 40, width: 40, backgroundColor: 'green' }}>40 x 40</view>
+      </flex>
+      , context
+    );
+  }, 0);
+  
+  return canvas;
+};
+
+export const justifyContentSpaceBetween = () => {
+  const { context, canvas } = requestNewContext();
+
+  setTimeout(() => {
+    ReactCanvas.render(
+      <flex style={{ height: 200, width: 200, justifyContent: 'space-between' }}>
+        <view style={{ height: 60, width: 60, backgroundColor: 'red' }}>60 x 60</view>
+        <view style={{ height: 20, width: 100, backgroundColor: 'blue' }}>20 x 100</view>
+        <view style={{ height: 40, width: 40, backgroundColor: 'green' }}>40 x 40</view>
       </flex>
       , context
     );
@@ -28,7 +45,7 @@ export const directionRow = () => {
       <flex style={{ height: 200, width: 200, flexDirection: 'row' }} >
         <view style={{ height: 60, width: 60, backgroundColor: 'red' }}>60 x 60</view>
         <view style={{ height: 20, width: 100, backgroundColor: 'blue' }}>20 x 100</view>
-        <view style={{ height: 60, width: 60, backgroundColor: 'green' }}>60 x 60</view>
+        <view style={{ height: 40, width: 40, backgroundColor: 'green' }}>40 x 40</view>
       </flex>
       , context
     );
